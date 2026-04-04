ff78fdfec944673cfec11624322a410bde7d8c4f
Ikhlas Fragrance <ikhlasfragrance@dev.local>
2026-04-04 12:04:49 +0530

    Fix CustomerController 500 error by adding orders relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,9 @@ class User extends Authenticatable
     {
         return (bool) $this->is_admin;
     }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
